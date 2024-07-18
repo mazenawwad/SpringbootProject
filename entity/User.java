90a09aaca6c6e117815ff00aa87f80933dc21ec4
mazenawwad <132779690+mazenawwad@users.noreply.github.com>
2024-07-18 03:29:36 +0300

    Update User.java
    Implemented the validation using the @Valid as well as importing different libraries for email, minimum, not null and size

--- entity/User.java
+++ entity/User.java
@@ -1,6 +1,10 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,8 +16,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "Name is mandatory")
+    @Size(min = 1, message = "Name must not be empty")
     private String name;
+
+    @NotNull(message = "Age is mandatory")
+    @Min(value = 1, message = "Age must be greater than 0")
     private Integer age;
+
+    @NotNull(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
     private String email;
 
     public Long getId() {
